d2dded98ff8df6d8c18e6a5f6e65322612eecc72
Bartek Kakol <kakolbartek25@gmail.com>
2025-03-07 14:23:20 +0100

    Dodana srednia

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,16 +1,27 @@
 ﻿#include <iostream>
 
+static double meth(int x[], int size) {
+    if (size == 0) return 0;
+
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += x[i];
+    }
+    return static_cast<double>(sum) / size;
+}
+
+
 int main()
 {
     std::cout << "Hello GitHub!\n";
 
-    for (int i = 0; i < 10; i++) {
-        std::cout << i << "\n";
-    }
-    int x = 0;
-    while (x != 100) {
-        x++;
-    }
+    int arr[] = { 1, 2, 3, 4, 5 };
+
+    double x = meth(arr, 5);
+
     std::cout << x;
+
 }
+
 
