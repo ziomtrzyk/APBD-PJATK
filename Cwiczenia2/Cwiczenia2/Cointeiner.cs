59662b7bd0569126a0df7b52f9ee78c4b88ee65a
Bartek Kakol <kakolbartek25@gmail.com>
2025-03-27 19:29:04 +0100

    Added Cointeiner.cs and OverfillException.cs

---
+++ Cwiczenia2/Cwiczenia2/Cointeiner.cs
@@ -0,0 +1,23 @@
+﻿namespace Cwiczenia2;
+
+public abstract class Cointeiner
+{
+    public double Weight{ get; set; }
+    public double Height { get; set; }
+    public double OwnWeight { get; set; }
+    public double Depth { get; set; }
+    public double MaxPayload { get; set; }
+    public string SerialNubmer { get; set; }
+
+    public void UnloadCargo()
+    {
+        Weight = 0;
+    }
+
+    public void LoadCargo(double weight)
+    {
+        if(weight > MaxPayload)
+            throw new OverfillException("The weight is greater than the max payload.");
+        Weight = weight;
+    }
+}
---
+++ Cwiczenia2/Cwiczenia2/OverfillException.cs
@@ -0,0 +1,7 @@
+﻿namespace Cwiczenia2;
+
+public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message)
+    {}
+}
